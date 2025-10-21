42d9065fea9b89c0e34bb50e5b9c0c1f7930a947
Alexander <st140044@student.spbu.ru>
2025-10-21 23:51:37 +0300

    Add file reading

--- Assignment2/main2a.cpp
+++ Assignment2/main2a.cpp
@@ -1,1 +1,21 @@
+/*
+ * Alexander Bushuev
+ * st140044@student.spbu.ru
+ * Home assignment 2a
+ * Main file
+ */
 
+#include <iostream>
+
+int main() {
+    std::ifstream infile;
+    infile.open("source.pdf",std::ios::binary|std::ios::in);
+    std::ofstream outfile;
+    std::outfile.open("temppdf.pdf",std::ios::binary|std::ios::out);
+    infile.read((char *)&buffer,sizeof(buffer))
+    outfile.write((char *)&buffer,sizeof(buffer));
+    infile.close();
+    outfile.close();
+
+    
+}
