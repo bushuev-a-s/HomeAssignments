c786bde709fad22acf2edfe6885a945f31299e60
Alexander <st140044@student.spbu.ru>
2025-12-04 22:54:12 +0300

    FINALLY IT WORKSgit add main2b.cppgit add main2b.cpp!

--- Assignment2/b/main2b.cpp
+++ Assignment2/b/main2b.cpp
@@ -7,41 +7,15 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
 int main() {
     int pointer=-1;
-    std::string temp;
-    std::string temp2;
-    std::string polish;
-    std::getline(std::cin,polish);
-    int spaces=0;
-    for (char ch:polish){
-        if (ch==' '){
-            spaces++;
-        }
-    }
-    float* stack=new float[spaces+1];
-    std::string* array=new std::string[spaces+1];
-    int cnt=0;
-    for (char ch:polish){
-        if (ch=='+'||ch=='-'||ch=='*'||ch=='/'){
-            temp2=ch;
-            array[cnt]=temp2;
-            cnt++;
-        }
-        else if (ch==' '){
-            array[cnt]=temp;
-            temp="";
-            cnt++;
-        }
-        else{
-            temp+=ch;
-        }
-    }
+    float* stack=new float[20];
     std::string data;
-    for (int i=0; i<spaces+1; i++){
-        data=array[i];
-        //std::cout<<data<<std::endl;
+    std::getline(std::cin, data);
+    std::istringstream iss(data);
+    while (iss>>data){
         if(data=="+"||data=="-"||data=="*"||data=="/"){
             pointer-=1;
             if (data=="+"){
@@ -56,13 +30,12 @@ int main() {
             if (data=="/"){
                 stack[pointer]=stack[pointer]/stack[pointer+1];
             }
-            //std::cout<<result<<std::endl;
         }
         else{
             pointer+=1;
             stack[pointer]=std::stof(data);
         }
     }
-    delete[] stack;
     std::cout<<stack[0];
+    delete[] stack;
 }
